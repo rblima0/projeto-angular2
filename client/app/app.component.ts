9c24b074a41b07f1e79641f3b11eac314a78310a
Rodrigo Banci <rblima0@gmail.com>
2017-08-18 10:09:47 -0300

    Adicionando estrutura e server para o projeto

---
+++ client/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+//anotacao do tipo Decorator
+@Component({
+    selector: 'app',
+    templateUrl: './app/app.component.html'
+})
+class AppComponent {
+
+}
