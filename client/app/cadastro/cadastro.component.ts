908e085dca1b4c14abeaa3a0828234205d6e6e8e
Rodrigo Banci <rblima0@gmail.com>
2017-08-28 11:17:11 -0300

    Formulario de Cadastro, associacao de eventos Data Binding / View para Dados e Dados para View

--- client/app/cadastro/cadastro.component.ts
+++ client/app/cadastro/cadastro.component.ts
@@ -1,8 +1,22 @@
 import { Component, Input } from '@angular/core';
+import { FotoComponent } from '../foto/foto.component';
 
 @Component({
     moduleId: module.id,
     selector: 'cadastro',
     templateUrl: './cadastro.component.html'
 })
-export class CadastroComponent {}
+export class CadastroComponent {
+    foto: FotoComponent = new FotoComponent();
+
+    constructor(){
+         this.foto.titulo = "Titulo";
+         this.foto.url = "url";
+         this.foto.descricao = "descricao";
+    }
+
+    cadastrar(event){
+        event.preventDefault();
+        console.log(this.foto);
+    }
+}
