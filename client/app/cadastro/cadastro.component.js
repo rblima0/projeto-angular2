e84557c1e94b3f40f0664871a68b2458fb046ac6
Rodrigo Banci <rblima0@gmail.com>
2017-08-28 13:35:44 -0300

    Ciclo de vida, Servico Http e Post

--- client/app/cadastro/cadastro.component.js
+++ client/app/cadastro/cadastro.component.js
@@ -10,13 +10,24 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 };
 var core_1 = require('@angular/core');
 var foto_component_1 = require('../foto/foto.component');
+var http_1 = require('@angular/http');
 var CadastroComponent = (function () {
-    function CadastroComponent() {
+    function CadastroComponent(http) {
         this.foto = new foto_component_1.FotoComponent();
+        this.http = http;
     }
     CadastroComponent.prototype.cadastrar = function (event) {
+        var _this = this;
         event.preventDefault();
         console.log(this.foto);
+        var headers = new http_1.Headers();
+        headers.append('Content-Type', 'application/json');
+        this.http
+            .post('v1/fotos', JSON.stringify(this.foto), { headers: headers })
+            .subscribe(function () {
+            _this.foto = new foto_component_1.FotoComponent();
+            console.log('Foto salva com sucesso !');
+        }, function (erro) { return console.log(erro); });
     };
     CadastroComponent = __decorate([
         core_1.Component({
@@ -24,7 +35,7 @@ var CadastroComponent = (function () {
             selector: 'cadastro',
             templateUrl: './cadastro.component.html'
         }), 
-        __metadata('design:paramtypes', [])
+        __metadata('design:paramtypes', [http_1.Http])
     ], CadastroComponent);
     return CadastroComponent;
 }());
